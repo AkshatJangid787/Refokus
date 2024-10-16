fa30f5874e874c778b2b6f659e428752d90c021a
AkshatJangid787 <akshatjangid14@gmail.com>
2024-10-16 20:48:20 +0530

    Changes in nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,8 @@ function Navbar() {
     <div className='max-w-screen-xl mx-auto py-6 flex items-center'>
         <img className='w-[5rem]' src={logo} alt="" />
         <div className='links flex gap-14 ml-24 pt-1'>
-            {["Home", "Work", "Culture"].map(elem => <a className='flex items-center gap-1' href='#'>
-                <span className='inline-block w-1 h-1 rounded-full bg-green-500 mb-1'></span>
+            {["Home", "Work", "Culture"].map((elem,index) => <a className='flex items-center gap-1' href='#'>
+              {index === 1 && (<span className='inline-block w-1 h-1 rounded-full bg-green-500 mb-1'></span>) }
                 {elem}</a>)}
         </div>
     </div>
