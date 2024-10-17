e9eb146a427a34cc8305862eab4cc08f7fb627d6
AkshatJangid787 <akshatjangid14@gmail.com>
2024-10-18 00:12:19 +0530

    work in progress

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { IoIosReturnRight } from 'react-icons/io'
 function Button() {
   return (
-    <div className='min-w-40 px-4 py-2 bg-zinc-100 text-black rounded-full flex items-center justify-between'>
+    <div className='min-w-40 px-4 py-2 bg-zinc-100 text-black rounded-full flex items-center justify-between '>
         <span className='text-sm   font-medium'>Get-started</span>
         <IoIosReturnRight className='ml-2 text-lg'/>
     </div>
---
+++ src/components/Stripes.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Stripe from './Stripe'
+
+function Stripes() {
+    var data = [
+        {url: "", number: 48},
+        {url: "", number: 2},
+        {url: "", number: 11},
+        {url: "", number: 48},
+        {url: "", number: 2},
+        {url: "", number: 11},
+    ]
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Stripes
