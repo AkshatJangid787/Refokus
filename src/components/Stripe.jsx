c9be3275b26ecdea21350944aa2d84587103d78e
AkshatJangid787 <akshatjangid14@gmail.com>
2024-10-18 00:44:53 +0530

    Stripes created

--- src/components/Stripe.jsx
+++ src/components/Stripe.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function Stripe() {
+function Stripe({val}) {
   return (
-    <div>Stripe</div>
+    <div className='w-[16.66%] px-4 py-5 border-t-[1.2px] border-b-[1.2px] border-r-[1.2px] bg-zinc-800 border-zinc-600 flex justify-between items-center'>
+        <img className="w-[7vw]"src={val.url} alt="" />
+        <span className='font-semibold'>{val.number}</span>
+    </div>
   )
 }
 
--- src/components/Stripes.jsx
+++ src/components/Stripes.jsx
@@ -3,16 +3,18 @@ import Stripe from './Stripe'
 
 function Stripes() {
     var data = [
-        {url: "", number: 48},
-        {url: "", number: 2},
-        {url: "", number: 11},
-        {url: "", number: 48},
-        {url: "", number: 2},
-        {url: "", number: 11},
+        {url: "https://cdn.prod.website-files.com/625e8524a4e1135fbab93b78/625e8524a4e113b5c0b93d07_mural.png", number: 48},
+        {url: "https://cdn.prod.website-files.com/625e8524a4e1135fbab93b78/625e8524a4e1134990b93d28_superai.png", number: 2},
+        {url: "https://cdn.prod.website-files.com/625e8524a4e1135fbab93b78/625e8524a4e1138207b93d2b_Rocketchat.png", number: 11},
+        {url: "https://cdn.prod.website-files.com/625e8524a4e1135fbab93b78/625e8524a4e113de2fb93d2c_haufe.png", number: 48},
+        {url: "https://cdn.prod.website-files.com/625e8524a4e1135fbab93b78/625e8524a4e11332e1b93d26_maxxis.png", number: 2},
+        {url: "https://cdn.prod.website-files.com/625e8524a4e1135fbab93b78/625e8524a4e1135966b93d24_rentspree.png", number: 11},
     ]
   return (
-    <div>
-        
+    <div className='flex items-center mt-20'>
+        {data.map((elem, index)=>(
+            <Stripe val={elem}/>
+        ))}
     </div>
   )
 }
