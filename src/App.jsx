e83fc5bd9069cb0908b47fcb995a6f1d4d121e9e
AkshatJangid787 <akshatjangid14@gmail.com>
2024-10-18 18:48:09 +0530

    product added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Work from './components/Work'
 import Stripes from './components/Stripes'
+import Products from './components/Products'
 
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-900 font-[satoshi] text-slate-100'>
+    <div className='w-full h-full bg-zinc-900 font-[satoshi] text-slate-100'>
       <Navbar/>
       <Work/>
       <Stripes/>
+      <Products/>
     </div>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Product from './Product'
 
 function Products() {
+  var products = [
+    {title: "arquitel", description: "Arqu Executive and its employees continue to receive orders for customers and get orders for customers", live: true, case: false},
+    {title: "TTR", description: "TTR Executive and its employees continue to receive orders for customers and get orders for customers", live: true, case: false},
+    {title: "YIR 2023", description: "YIR 2023 Executive and its employees continue to receive orders for customers and get orders for customers", live: true, case: true},
+    {title: "Yahoo", description: "Yahoo Executive and its employees continue to receive orders for customers and get orders for customers", live: true, case: false},
+    {title: "Bing", description: "Bing Executive and its employees continue to receive orders for customers and get orders for customers", live: true, case: false},
+  ]
+
   return (
-    <div>Products</div>
+    <div>
+        {products.map((val, index)=> <Product val={val}/>)}
+    </div>
   )
 }
 
