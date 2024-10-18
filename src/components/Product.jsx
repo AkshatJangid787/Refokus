c2e5532a8e388690c1e3588b67535cebf51d93b8
AkshatJangid787 <akshatjangid14@gmail.com>
2024-10-18 19:42:17 +0530

    changes in product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,7 +5,7 @@ function Product({val}) {
   return (
     <div className='w-full py-20 text-white'>
         <div className='max-w-screen-xl mx-auto flex items-center justify-between'>
-            <h1 className='text-6xl capitalize font-semibold'>{val.title}</h1>
+            <h1 className='text-6xl capitalize font-medium'>{val.title}</h1>
             <div className='dets w-1/3'>
                 <p>{val.description}</p>
                 <Button/>
